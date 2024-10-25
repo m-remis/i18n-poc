a8c41485a80541cad9a720f898ccaf5be3060c9a
Michal Remis <michal.remis@globallogic.com>
2024-10-26 00:35:02 +0200

    cosmetic changes

--- src/main/java/com/michal/examples/i18n/controller/ResourceController.java
+++ src/main/java/com/michal/examples/i18n/controller/ResourceController.java
@@ -19,9 +19,9 @@ public class ResourceController {
 
     public static final String CONTROLLER_PREFIX = "/resources";
 
-    public static final String MESSAGES_FOR_MODULE_AND_LANGUAGE = "/i18n/module/{module}/language/{language}";
-    public static final String MESSAGES_FOR_MODULE = "/i18n/module/{module}";
-    public static final String I18N_LANGUAGE = "/i18n/language/{language}";
+    public static final String I18N_MESSAGES_FOR_MODULE_AND_LANGUAGE = "/i18n/module/{module}/language/{language}";
+    public static final String I18N_MESSAGES_FOR_LANGUAGE = "/i18n/language/{language}";
+    public static final String I18N_MESSAGES_FOR_MODULE = "/i18n/module/{module}";
     public static final String I18N_MESSAGES = "/i18n";
 
     private final LanguageService languageService;
@@ -32,29 +32,29 @@ public class ResourceController {
 
     @GetMapping(I18N_MESSAGES)
     @Operation(summary = "Returns list of all available resources")
-    public ResponseEntity<List<LanguageResourceDto>> getAllForModule(@RequestParam(value = "customer_id", required = false) String customerId) {
+    public ResponseEntity<List<LanguageResourceDto>> getAllResources(@RequestParam(value = "customer_id", required = false) String customerId) {
         return ResponseEntity.ok(languageService.getAll(customerId));
     }
 
-    @GetMapping(MESSAGES_FOR_MODULE)
-    @Operation(summary = "Returns list of all available resources for module")
-    public ResponseEntity<List<LanguageResourceDto>> getAllForModule(@PathVariable("module") String module,
-                                                                     @RequestParam(value = "customer_id", required = false) String customerId) {
+    @GetMapping(I18N_MESSAGES_FOR_MODULE)
+    @Operation(summary = "Returns list of all available resources for a specific module")
+    public ResponseEntity<List<LanguageResourceDto>> getAllResourcesForModule(@PathVariable("module") String module,
+                                                                              @RequestParam(value = "customer_id", required = false) String customerId) {
         return ResponseEntity.ok(languageService.getAllForModule(module, customerId));
     }
 
-    @GetMapping(MESSAGES_FOR_MODULE_AND_LANGUAGE)
-    @Operation(summary = "Returns all resources for module and language")
-    public ResponseEntity<List<LanguageResourceDto>> getAllForModuleAndLanguage(@PathVariable("module") String module,
-                                                                                @PathVariable("language") String languageCode,
-                                                                                @RequestParam(value = "customer_id", required = false) String customerId) {
+    @GetMapping(I18N_MESSAGES_FOR_MODULE_AND_LANGUAGE)
+    @Operation(summary = "Returns all resources for a specific module and language")
+    public ResponseEntity<List<LanguageResourceDto>> getAllResourcesForModuleAndLanguage(@PathVariable("module") String module,
+                                                                                         @PathVariable("language") String languageCode,
+                                                                                         @RequestParam(value = "customer_id", required = false) String customerId) {
         return ResponseEntity.ok(languageService.getAllForModuleAndLanguage(module, languageCode, customerId));
     }
 
-    @GetMapping(I18N_LANGUAGE)
-    @Operation(summary = "Returns all language resources for customer and chosen language")
-    public ResponseEntity<List<LanguageResourceDto>> getAllForLanguage(@RequestParam(value = "customer_id", required = false) String customerId,
-                                                                       @PathVariable("language") String language) {
+    @GetMapping(I18N_MESSAGES_FOR_LANGUAGE)
+    @Operation(summary = "Returns all language resources for a chosen language")
+    public ResponseEntity<List<LanguageResourceDto>> getAllResourcesForLanguage(@RequestParam(value = "customer_id", required = false) String customerId,
+                                                                                @PathVariable("language") String language) {
         return ResponseEntity.ok(languageService.getAllForLanguage(customerId, language));
     }
 }
